f3ef3b3907e8e26928348d54c052d9b4703be01c
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-06-03 18:32:15 +0200

    walczymy dalej ide pobiegać

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -5,62 +5,149 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Numero workWithNewNumber = new Numero(sc.nextInt());
+        System.out.println( workWithNewNumber.toString());
 
-        System.out.println("Enter a natural number:");
-        String input = sc.next();
-        int inputAsInt = Integer.parseInt(input);
+//        System.out.println("Enter a natural number:");
+//        String input = sc.next();
+//        int inputAsInt = Integer.parseInt(input);
+//
+//        if (inputAsInt == 0) {
+//            System.out.println(inputAsInt + " is not natural.");
+//            System.exit(1);
+//        } else if (inputAsInt < 0) {
+//            System.out.println("This number is not natural!");
+//            System.exit(1);
+//        }
 
-        if (inputAsInt == 0) {
-            System.out.println(inputAsInt + " is not natural.");
-            System.exit(1);
-        } else if (inputAsInt < 0) {
-            System.out.println("This number is not natural!");
-            System.exit(1);
+
+    }
+}
+
+class Numero {
+
+    int numberInt;
+    boolean even;
+    boolean odd;
+    boolean buzz;
+    boolean duck;
+
+    public Numero(int number) {
+        this.numberInt = number;
+    }
+
+    public void isEvenOrOddMethod(int n) {
+        if (n % 2 == 0) {
+            setEven(true);
+            setOdd(false);
         }
-
-
-        String OddOrEven = "Odd";
-        if (inputAsInt % 2 == 0) {
-            OddOrEven = "Even";
+        else {
+            setEven(false);
+            setOdd(true);
         }
+    }
+
+    public void isDuckMethod(int n) {
+        System.out.println("pusta metoda - drukuje liczbe: " + n);
+    }
+
+    public void isBuzzMethod(int n) {
+
 
         boolean isBuzzNumber = false;
         boolean endsWithSeven = false;
         boolean divisibleBy7 = false;
 
-        if (input.charAt(input.length() - 1) == '7') {
-            isBuzzNumber = true;
-            endsWithSeven = true;
+        if (String.valueOf(n).charAt(String.valueOf(n).length() - 1) == '7') {
+            setBuzz(true);
+         //   endsWithSeven = true;
         }
-        if (inputAsInt % 7 == 0) {
-            isBuzzNumber = true;
-            divisibleBy7 = true;
-        }
+        if (n % 7 == 0) {
+            setBuzz(true);
+//            divisibleBy7 = true;
+//        }
 
-        String secondline = isBuzzNumber ? "It is a Buzz number.\n" : "It is not a Buzz number.\n";
+//     String secondline = isBuzzNumber ? "It is a Buzz number.\n" : "It is not a Buzz number.\n";
+//
+//      String explanation = "";
+//        if (isBuzzNumber == false) {
+//          //  explanation = " is neither divisible by 7 nor it ends with 7.";
+//        }
+//        if (isBuzzNumber && endsWithSeven) {
+//       //     explanation = " is ends with 7";
+//        }
+//        if (isBuzzNumber && divisibleBy7) {
+//        //    explanation = " is divisible by 7.";
+//        }
+//        if (isBuzzNumber && endsWithSeven && divisibleBy7) {
+//       //     explanation = " is divisible by 7 and it ends with 7.";
+//        }
 
-        String explanation = "";
-        if (isBuzzNumber == false) {
-            explanation = " is neither divisible by 7 nor it ends with 7.";
-        }
-        if (isBuzzNumber && endsWithSeven) {
-            explanation = " is ends with 7";
-        }
-        if (isBuzzNumber && divisibleBy7) {
-            explanation = " is divisible by 7.";
-        }
-        if (isBuzzNumber && endsWithSeven && divisibleBy7) {
-            explanation = " is divisible by 7 and it ends with 7.";
-        }
-
-
-        String lastLine = input + explanation;
-
-        System.out.println("This number is " + OddOrEven + ".\n" +
-                secondline +
-                "Explanation:\n" +
-                lastLine);
-
+//
+//        String lastLine = n + explanation;
+//
+//        System.out.println("This number is " + OddOrEven + ".\n" +
+//                secondline +
+//                "Explanation:\n" +
+//                lastLine);
 
     }
+
+}
+
+
+    @Override
+    public String toString() {
+        isEvenOrOddMethod(numberInt);
+        isBuzzMethod(numberInt);
+        isDuckMethod(numberInt);
+        return "Property of " + getNumber() +
+                "\neven: " + even +
+                "\nodd: " + odd +
+                "\nbuzz: " + buzz +
+                "\nduck :" + duck;
+    }
+
+
+    public int getNumber() {
+        return numberInt;
+    }
+
+    public void setNumber(int number) {
+        this.numberInt = number;
+    }
+
+    public boolean isEven() {
+        return even;
+    }
+
+    public void setEven(boolean even) {
+        this.even = even;
+    }
+
+    public boolean isOdd() {
+        return odd;
+    }
+
+    public void setOdd(boolean odd) {
+        this.odd = odd;
+    }
+
+    public boolean isBuzz() {
+        return buzz;
+    }
+
+    public void setBuzz(boolean buzz) {
+        this.buzz = buzz;
+    }
+
+    public boolean isDuck() {
+        return duck;
+    }
+
+    public void setDuck(boolean duck) {
+        this.duck = duck;
+    }
+
+
 }
